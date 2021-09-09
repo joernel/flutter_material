11d4f76b7846cc136fc620f19680a99e0d8acad4
jzinsser <joern.zinsser@bridging-it.de>
2021-09-09 14:28:13 +0200

    End of material_1 and start of material_2

--- lib/main.dart
+++ lib/main.dart
@@ -46,12 +46,53 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    final _usernameController = TextEditingController();
+    final _passwordController = TextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-          child: Text('')
-    ));
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SafeArea(
+            child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: [
+            const SizedBox(height: 80.0),
+            Column(
+              children: [Image.asset('assets/diamond.png'), Text('LOGIN')],
+            ),
+            const SizedBox(height: 80.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: false,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: false,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: [
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();         },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'))
+              ],
+            )
+          ],
+        )));
   }
 }
